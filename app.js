85bf116370105e3503e2b6a165aece666709540f
Sarthak Sadh <sarthaksadh@Sarthaks-MacBook-Pro-5195.local>
2019-03-09 23:06:57 +0530

    for heroku

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const movieTrailer = require('movie-trailer');
 
 var port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true });
+mongoose.connect('mongodb://sadh:sarthak01@ds157742.mlab.com:57742/vod', { useMongoClient: true });
+//mongoose.connect('mongodb://sadh:sarthak01@ds157742.mlab.com:57742/vod', { useNewUrlParser: true });
+
+mongoose.connection.on("error", function (err) {
+    console.log("Could not connect to mongo server!");
+    return console.log(err);
+});
+
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 } }))
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -53,22 +61,22 @@ var Movies = require('./models/movies');
 
 //     });
 // }
-//  var p = './public/data/prime.json';
-//  var data = JSON.parse(fs.readFileSync(p));
-// for(var i=0;i<data.length;i++){
-//     var _movie = new Movies({
-//         "title": data[i].Title,
-//         "poster": "",
-//         "video": "",
-//         "rating": data[i].rating,
-//         "provider":"amazon prime"
-//     });
-//     _movie.save((err) => {
-//         console.log(`saved ${i}`);
+ var p = './public/data/prime.json';
+ var data = JSON.parse(fs.readFileSync(p));
+for(var i=0;i<data.length;i++){
+    var _movie = new Movies({
+        "title": data[i].Title,
+        "poster": "",
+        "video": "",
+        "rating": data[i].rating,
+        "provider":"amazon prime"
+    });
+    _movie.save((err) => {
+        console.log(`saved ${i}`);
 
 
-//     });
-// }
+    });
+}
 
 
 
@@ -114,7 +122,7 @@ app.get('/api/hotstar', (req, res, next) => {
 app.get('/api/search/:id', function (req, res) {
     var id = req.params.id;
     console.log(id);
-    Movies.findOne({ title: new RegExp('^' + id + '$', "i") }, function (err, movies) {
+    Movies.findOne({"title":{"$regex":id, $options:"i"} }, function (err, movies) {
         res.send(movies);
         console.log(movies);
     });
